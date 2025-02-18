fca1fc65c8339b3feee05218e4c704641786bc63
aaryadevchandra <aaryadevc@gmail.com>
2025-02-18 10:33:46 +0530

    changed datepicker

--- src/components/testPages/FillupPage.jsx
+++ src/components/testPages/FillupPage.jsx
@@ -1,17 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CalibrationPage from "./CalibrationPage";
-import DatePicker from 'rsuite/DatePicker';
-
-// (Optional) Import component styles. If you are using Less, import the `index.less` file. 
-import 'rsuite/DatePicker/styles/index.css';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { differenceInYears, differenceInMonths } from "date-fns";
 import { useNavigate, useLocation } from "react-router-dom";
 import { AppContext } from "../aignosisintegration/AppContext";
-import { data } from "autoprefixer";
 import { format } from "date-fns";
 
 export const FillupPage = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
   const [isBackInfoVisible, setIsBackInfoVisible] = useState(false);
   const [dob, setDob] = useState(null);
   const [, setAgeYears] = useState("");
@@ -20,7 +19,6 @@ export const FillupPage = () => {
   const [dataCollectionMode, setDataCollectionMode] = useState([]); // New state for selected options
   const navigate = useNavigate(); // Initialize the useNavigate hook
   const { testData, setTestData } = useContext(AppContext);
-  const [age, setAge] = useState(null);
   const [isButtonsDisabled, setIsButtonsDisabled] = useState(true);
   const [buttonAccessibility, setButtonAccessibility] = useState({
     INCLEN: true,
@@ -28,7 +26,7 @@ export const FillupPage = () => {
     CARS: true,
   });
 
-  console.log(testData,"testData");
+  console.log(testData, "testData");
 
   useEffect(() => {
     // if (testData.PATIENT_UID === "" || testData.TRANSACTION_ID == "") {
@@ -41,35 +39,24 @@ export const FillupPage = () => {
   }, []);
 
   useEffect(() => {
-    if (dob) {
-      const years = differenceInYears(new Date(), dob);
-      setAge(years);
-      setIsButtonsDisabled(false);
 
-      setButtonAccessibility({
-        INCLEN: years < 2 || years > 9,
-        ISAA: years < 3,
-        CARS: years < 2,
-      });
-      console.log(years);
-    }
-    
+    setDob(formatDate(selectedDate));
+
     // Push initial state to prevent default navigation
     window.history.pushState(null, null, window.location.href);
-  
+
     const handleBackButton = () => {
       navigate("/calibrationpage"); // Redirect to CalibrationPage on back press
     };
-  
+
     // Listen for the popstate event
     window.addEventListener("popstate", handleBackButton);
-  
+
     // Cleanup the listener on unmount
     return () => {
       window.removeEventListener("popstate", handleBackButton);
     };
-  }, [navigate,dob]);
-  
+  }, [navigate, selectedDate]);
 
   const handleNextClick = async () => {
     try {
@@ -90,7 +77,6 @@ export const FillupPage = () => {
           patientDOB: dob.toString(),
         });
 
-        // console.log("going to  data collection", testData.dataCollectionMode);
         navigate("/autismtest");
       }
     } catch (error) {
@@ -99,6 +85,11 @@ export const FillupPage = () => {
     }
   };
 
+
+  function formatDate(date){
+    const formattedDate = date ? format(date, "ddMMyyyy") : "";
+    return formattedDate.toString();
+  }
 
   // Function to calculate age based on DOB
   const handleDateChange = (date) => {
@@ -116,24 +107,6 @@ export const FillupPage = () => {
     console.log("Formatted DOB:");
   };
 
-  // // Function to handle checkbox change
-  // const handleCheckboxChange = (event) => {
-  //   const { value, checked } = event.target;
-  //   setDataCollectionMode((prev) => {
-  //     const updatedData = checked
-  //       ? [...prev, value]
-  //       : prev.filter((item) => item !== value);
-
-  //     setTestData((prevTestData) => ({
-  //       ...prevTestData,
-  //       dataCollectionMode: updatedData, // Use the updated state here
-  //     }));
-
-  //     console.log(updatedData);
-  //     return updatedData;
-  //   });
-  // };
-
   const handleCheckboxChange = (event) => {
     const { value, checked } = event.target;
     console.log(dataCollectionMode);
@@ -141,14 +114,14 @@ export const FillupPage = () => {
       const updatedDataCollectionMode = checked
         ? [...prevTestData.dataCollectionMode, value] // Add value if checked
         : prevTestData.dataCollectionMode.filter((item) => item !== value); // Remove value if unchecked
-  
+
       return {
         ...prevTestData,
         dataCollectionMode: updatedDataCollectionMode,
       };
     });
   };
-  
+
   return (
     <>
       <div className="bg-[#1A0C25] flex flex-col justify-center items-center h-[110vh] ">
@@ -188,30 +161,11 @@ export const FillupPage = () => {
               </p>
 
               <form className="space-y-4">
-                {/* <select className="bg-[#1A0C25] text-white px-4 py-2.5 rounded-lg w-full border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500">
-                  <option className="bg-[#1A0C25]">Choose Language</option>
-                  <option className="bg-[#1A0C25]" value="en">
-                    English
-                  </option>
-                  <option className="bg-[#1A0C25]" value="es">
-                    Spanish
-                  </option>
-                  <option className="bg-[#1A0C25]" value="fr">
-                    French
-                  </option>
-                  <option className="bg-[#1A0C25]" value="de">
-                    German
-                  </option>
-                </select> */}
-
                 <input
                   id="patient-name-input"
                   type="text"
                   placeholder="Patient Name"
                   className="bg-[#1A0C25] text-white px-4 py-2.5 rounded-lg w-full placeholder-gray-500 border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500"
-                  // onChange={(e) => {
-                  //   setPName(e.target.value);
-                  // }}
                 />
 
                 {/* <input
@@ -222,94 +176,28 @@ export const FillupPage = () => {
                 /> */}
 
                 {/* Date Picker for DOB */}
-                <DatePicker
+                {/* <DatePicker
                   selected={dob}
                   onChange={handleDateChange}
                   placeholder="Patient DOB"
                   // style={backgroundColor: "transparent",}
                   className="bg-[#1A0C25] text-white px-4 py-2.5 w-25 rounded-lg w-full placeholder-gray-500 border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500"
-                />
-                {/* <DatePicker oneTap style={{ width: 200 }} onChange={handleDateChange} className="bg-[#1A0C25] text-white px-4 py-2.5 rounded-lg w-full placeholder-gray-500 border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500 "/> */}
-                
-
-                {/* Automatically populated fields for age */}
-                {/* <input
-                  type="text"
-                  placeholder="Patient Age (Years)"
-                  value={ageYears}
-                  readOnly
-                  className="bg-[#1A0C25] text-white px-4 py-2.5 rounded-lg w-full placeholder-gray-500 border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Patient Age (Months)"
-                  value={ageMonths}
-                  readOnly
-                  className="bg-[#1A0C25] text-white px-4 py-2.5 rounded-lg w-full placeholder-gray-500 border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Patient Year"
-                  value={ageFullYear}
-                  readOnly
-                  className="bg-[#1A0C25] text-white px-4 py-2.5 rounded-lg w-full placeholder-gray-500 border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500"
                 /> */}
-                
-                {/* Data Collection Mode Section */}
-                {/* <div className="text-white">
-                  <h3 className="font-semibold mb-2 text-sm">
-                    Data Collection Mode
-                  </h3>
-                  <div className="space-y-2">
-                    <label className="flex items-center space-x-2" style={{marginLeft:'-63%',}}>
-                      <input
-                        type="checkbox"
-                        value="INCLEN"
-                        onChange={handleCheckboxChange}
-                        disabled={isButtonsDisabled || buttonAccessibility.INCLEN}
-                  className={`${
-                    buttonAccessibility.INCLEN
-                      ? "bg-gray-500"
-                      : "bg-blue-500 hover:bg-blue-700"
-                  } text-white px-4 py-2 rounded-lg w-full`}
-                      />
-                      <span style={{marginLeft:'-47%',}}>INCLEN</span>
-                    </label>
-                    <label className="flex items-center space-x-2" style={{marginLeft:'-63%',}}>
-                      <input
-                        type="checkbox"
-                        value="ISAA"
-                        onChange={handleCheckboxChange}
-                        disabled={isButtonsDisabled || buttonAccessibility.ISAA}
-                  className={`${
-                    buttonAccessibility.ISAA
-                      ? "bg-gray-500"
-                      : "bg-blue-500 hover:bg-blue-700"
-                  } text-white px-4 py-2 rounded-lg w-full`}
-                      />
-                      <span style={{marginLeft:'-47%',}}>ISAA</span>
-                    </label>
-                    <label className="flex items-center space-x-2 " style={{marginLeft:'-63%',}}>
-                      <input
-                        type="checkbox"
-                        value="CARS"
-                        disabled={isButtonsDisabled || buttonAccessibility.CARS}
-                        onChange={handleCheckboxChange}
-                        className={`${
-                          buttonAccessibility.CARS
-                            ? "bg-gray-500"
-                            : "bg-blue-500 hover:bg-blue-700"
-                        } text-white px-4 py-2 rounded-lg w-full`}
-                      />
-                      <span style={{marginLeft:'-47%',}}>CARS</span>
-                    </label>
-                  </div>
-                </div>
-                <h3 className="font-semibold mb-2 text-sm text-white">
-                    Fill Google Form Instead? <a href="https://docs.google.com/forms/d/e/1FAIpQLSd_dXebCWKaocA7KpAxWJAyHGfEwsqiDvAgXk0tj4ZQa0bYhg/viewform">click here</a>
-                  </h3>       */}
+
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    label="Select a date"
+                    value={selectedDate}
+                    onChange={(newValue) => setSelectedDate(newValue)}
+                    renderInput={(params) => <TextField {...params} />}
+                    sx={{
+                      backgroundColor: "white"
+                    }}
+                  />
+                </LocalizationProvider>
+
+                {/* <DatePicker oneTap style={{ width: 200 }} onChange={handleDateChange} className="bg-[#1A0C25] text-white px-4 py-2.5 rounded-lg w-full placeholder-gray-500 border-[#B7407D4D] focus:outline-none focus:ring-2 focus:ring-pink-500 "/> */}
+
                 <div className="flex justify-center items-center gap-2 max-sm:flex-col">
                   <Link
                     to="/prices"
